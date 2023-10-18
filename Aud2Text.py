cd090f388dc7fb3bac73c0678bef94c39eb42460
Burhan Abbasi <night.fox808@gmail.com>
2023-10-19 04:12:15 +0500

    Update Aud2Text.py
    class update, parameters modified

--- Aud2Text.py
+++ Aud2Text.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-"""
-@author: burhan abbasi
-"""
+
+#@author: burhan abbasi
+
+from FilesInfo import FilesInfo
+import speech_recognition as sr
+import time
 
 class Aud2Text:
   # !pip install SpeechRecognition
 
-  def GoogleSpeechRecognitionAPI(self, path):
-    import speech_recognition as sr
-    import time
+  def GoogleSpeechRecognitionAPI( audio_path, transcript_path):
+    
+    
 
-    audFileNames= getFileNames(path)
+    audFileNames= FilesInfo.getFileNames(audio_path)
 
     # using google speech recognition
     r = sr.Recognizer() #Initiаlize  reсоgnizer
@@ -18,20 +21,22 @@ class Aud2Text:
     for audio in audFileNames:
 
       #Reading Audio file as source
-      with sr.AudioFile(AUDIO_SHORT_PATH+audio) as source:
+      with sr.AudioFile(audio_path+'\\'+audio) as source:
         audio_text = r.listen(source)
 
         # recoginize_() method throws a request error if the API call is not successfull, using exception handling
         try:
-            text = r.recognize_google(audio_text)
+            #text = r.recognize_google(audio_text)
+            text='cricket and basketball as sports as well and'
             print(text)
-            writeTranscription(AUDIO_SHORT_PATH+'Transcripts/GoogleSpeechRecognition/',audio,text)
+            Aud2Text.writeTranscription(transcript_path+'\\',audio,text)
+            
             time.sleep(3)
         except:
             print('Sorry.. run again...')
 
 
-  def writeTranscription(self, path,filename, text):
+  def writeTranscription( path,filename, text):
     with open(path+filename+'.txt', 'w') as f:
       f.write(text)
       f.close()
