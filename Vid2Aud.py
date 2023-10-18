0213f5daee55a66a2802309b0859ace650acc2ce
Burhan Abbasi <night.fox808@gmail.com>
2023-10-19 04:13:59 +0500

    Update Vid2Aud.py
    class updated,
    parameters and function calls

--- Vid2Aud.py
+++ Vid2Aud.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
-"""
-@author: burhan abbasi
-"""
 
-import moviepy.editor as VideoFileClip
-        
+#@author: burhan abbasi
+
+from moviepy.editor import VideoFileClip
+from FilesInfo import FilesInfo  
+      
 class Vid2Aud:
-    def __init__(self, name):
-        self.outputFolder= "audio-extract/"
+    outputFolder= "\\audio-extract\\"
+    # def __init__(self):
+        # self.
     
-    def extractAudioFromVideoFiles(self, path):
-        vidFileNames= getFileNames(path)
-
+    def extractAudioFromVideoFiles(path):
+        vidFileNames= FilesInfo.getFileNames(path)
+        
+        print("FileNames in the location " +path+"  are :"   )
+        print(vidFileNames)
         for name in vidFileNames:
-          # Load the video
-          video = VideoFileClip(path+name)
-          # Extract the audio from the video
-          audio_file = video.audio
-          audio_file.write_audiofile(path+outputFolder+name+".wav")
-
+            if (name=="audio-extract" or name=="transcript"):
+                print(name)
+                continue
+            else:
+                # Load the video
+                print(path+"\\"+name)
+                video = VideoFileClip(path+"\\"+name)
+                # Extract the audio from the video
+                audio_file = video.audio
+                audio_file.write_audiofile(path+Vid2Aud.outputFolder+name+".wav")
